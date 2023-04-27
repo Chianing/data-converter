a50c0085a4ccec33d1ea3c565c06d2aff0ad9611
Chianing <chianing9527@gmail.com>
2023-04-27 14:11:09 +0800

    build: add github publish config

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,21 @@ plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.7.20"
     id("org.jetbrains.intellij") version "1.13.1"
+
+    `maven-publish`
 }
 
 group = "com.chianing.plugin"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
+    maven {
+        setUrl("https://maven.aliyun.com/repository/public/")
+    }
+    maven {
+        setUrl("https://maven.aliyun.com/repository/spring/")
+    }
+    mavenLocal()
     mavenCentral()
 }
 
@@ -54,4 +63,23 @@ dependencies {
     implementation("com.alibaba.fastjson2:fastjson2:2.0.29")
     // https://mvnrepository.com/artifact/com.alibaba/druid
     implementation("com.alibaba:druid:1.2.17")
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "DataConverterPackage"
+            url = uri("https://maven.pkg.github.com/Chianing/data-converter")
+            credentials {
+                System.getenv().values.toString()
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,9 @@
 rootProject.name = "data-converter"
+
+pluginManagement {
+    repositories {
+        maven {
+            setUrl("https://maven.aliyun.com/repository/gradle-plugin")
+        }
+    }
+}
